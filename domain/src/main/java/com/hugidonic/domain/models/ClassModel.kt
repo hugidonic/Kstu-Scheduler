b670f3f61fb87b907f2e248736d6948e9f21965e
Hugidonic <netvaca@ya.ru>
2023-08-12 19:40:32 +0300

    [repository]: Models refactored

---
+++ domain/src/main/java/com/hugidonic/domain/models/ClassModel.kt
@@ -0,0 +1,7 @@
+package com.hugidonic.domain.models
+
+data class ClassModel(
+    val subject: SubjectModel,
+    val dayOfWeek: String,
+    val orderIndex: Int,
+)
--- domain/src/main/java/com/hugidonic/domain/models/ScheduleDayModel.kt
+++ domain/src/main/java/com/hugidonic/domain/models/ScheduleDayModel.kt
@@ -1,8 +1,7 @@
 package com.hugidonic.domain.models
 
 data class ScheduleDayModel(
-	val dayOfWeek: String,
-	val typeOfWeek: String,
-	val date: String,
-	val subjects: List<SubjectModel?>?,
+    val dayOfWeek: String,
+    val typeOfWeek: String,
+    val date: String,
 )
