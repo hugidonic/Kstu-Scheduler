577cde3f1f0b986ca2ee8becd970f43a573cd02e
Hugidonic <netvaca@ya.ru>
2023-09-14 13:57:43 +0300

    [test]/{data}: Database dao's and entity converters tests

--- data/src/androidTest/java/com/hugidonic/data/db/converters/ScheduleConverterTest.kt
+++ data/src/androidTest/java/com/hugidonic/data/db/converters/ScheduleConverterTest.kt
@@ -1,10 +1,11 @@
 package com.hugidonic.data.db.converters
 
 import com.google.common.truth.Truth
+import com.hugidonic.data.converters.toModel
 import com.hugidonic.data.converters.toScheduleDayEntity
-import com.hugidonic.data.converters.toScheduleDayModel
 import com.hugidonic.data.database.entities.ScheduleDayEntity
-import com.hugidonic.domain.models.ScheduleDayModel
+import com.hugidonic.data.database.entities.relations.ScheduleDayWithSubjects
+import com.hugidonic.data.remote.dto.ScheduleDayDto
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -12,31 +13,39 @@ class ScheduleConverterTest {
     @Test
     @DisplayName("Should convert from entity to Model")
     fun entityToScheduleDayModelConverter() {
-        val entity = ScheduleDayEntity(
-            dayOfWeek = "Пн",
-            typeOfWeek = "Нечет",
-            date = "09.11.2020"
+        val entity = ScheduleDayWithSubjects(
+            scheduleDayInfo = ScheduleDayEntity(
+                dayOfWeek = "Пн",
+                typeOfWeek = "Нечет",
+                date = "09.11.2020",
+                scheduleDayId = "Нечет/Пн"
+            ),
+            subjects = emptyList()
         )
 
-        val model = entity.toScheduleDayModel()
+        val model = entity.toModel()
         Truth.assertThat(model.date).isEqualTo("09.11.2020")
         Truth.assertThat(model.typeOfWeek).isEqualTo("Нечет")
         Truth.assertThat(model.dayOfWeek).isEqualTo("Пн")
+        Truth.assertThat(model.scheduleDayId)
+            .isEqualTo("${entity.scheduleDayInfo.typeOfWeek}/${entity.scheduleDayInfo.dayOfWeek}")
     }
 
     @Test
     @DisplayName("Should convert from dto to Entity")
     fun dtoToScheduleDayEntityConverter() {
-        val dto = ScheduleDayModel(
+        val dto = ScheduleDayDto(
             dayOfWeek = "Ср",
             typeOfWeek = "Нечет",
-            date = "09.11.2020"
+            date = "09.11.2020",
+            subjects = emptyList()
         )
 
         val entity = dto.toScheduleDayEntity()
 
         Truth.assertThat(entity.date).isEqualTo("09.11.2020")
         Truth.assertThat(entity.typeOfWeek).isEqualTo("Нечет")
-        Truth.assertThat(entity.dayOfWeek.toString()).isEqualTo("Ср")
+        Truth.assertThat(entity.dayOfWeek).isEqualTo("Ср")
+        Truth.assertThat(entity.scheduleDayId).isEqualTo("${dto.typeOfWeek}/${dto.dayOfWeek}")
     }
 }
---
+++ data/src/androidTest/java/com/hugidonic/data/db/scheduleDao/schedule/ScheduleDayDaoTest.kt
@@ -0,0 +1,70 @@
+package com.hugidonic.data.db.scheduleDao.schedule
+
+import android.util.Log
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth
+import com.hugidonic.data.converters.toScheduleDayEntity
+import com.hugidonic.data.database.AppDatabase
+import com.hugidonic.data.database.ScheduleDao
+import com.hugidonic.data.database.entities.ScheduleDayEntity
+import com.hugidonic.data.db.scheduleDao.dummyData.DummyData
+import com.hugidonic.data.remote.ApiFactory
+import com.hugidonic.data.remote.ApiService
+import com.hugidonic.domain.models.SubjectModel
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class ScheduleDayDaoTest {
+    private lateinit var db: AppDatabase
+    private lateinit var dao: ScheduleDao
+    private lateinit var api: ApiService
+    private lateinit var subjects: List<SubjectModel>
+
+    @BeforeEach
+    fun setUp() {
+        db = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            AppDatabase::class.java
+        )
+            .allowMainThreadQueries()
+            .build()
+        dao = db.scheduleDao()
+        api = ApiFactory().scheduleApi
+    }
+
+    @Test
+    @DisplayName("Should save schedule day to db")
+    fun should_save_schedule_day_to_db() = runTest {
+        val scheduleDayId = "Чет/Пн"
+        val scheduleDayEntity = ScheduleDayEntity(
+            dayOfWeek = "Пн",
+            typeOfWeek = "Чет",
+            date = "12.02.2023",
+            scheduleDayId = scheduleDayId
+        )
+
+        dao.insertScheduleDay(scheduleDayEntity)
+
+        val scheduleDayFromDb = dao.getScheduleDay(scheduleDayId = scheduleDayId)
+
+        Truth.assertThat(scheduleDayFromDb?.scheduleDayInfo).isEqualTo(scheduleDayEntity)
+    }
+
+    @Test
+    @DisplayName("Should save list of schedule days to db")
+    fun should_save_list_of_schedule_days_to_db() = runTest {
+        val weekScheduleEntities = DummyData.weekSchedule.map { it.toScheduleDayEntity() }
+        dao.insertWeekSchedule(weekScheduleEntities)
+
+        val typeOfWeek = weekScheduleEntities[0].typeOfWeek
+        val weekFromDb = dao.getWeekScheduleByType(typeOfWeek = typeOfWeek)
+        Truth.assertThat(weekFromDb).isNotEmpty()
+        weekFromDb.forEach {
+            Truth.assertThat(it.scheduleDayInfo).isNotNull()
+            Truth.assertThat(it.subjects).isNotNull()
+        }
+    }
+}
