25c543d3a2f95ec9707bb0ef1284003f67fe9945
Hugidonic <netvaca@ya.ru>
2023-09-14 13:38:40 +0300

    [dev]/{data}: ScheduleRepositoryImpl works kinda well, but I think there are some bugs.

--- data/src/main/java/com/hugidonic/data/repository/ScheduleRepositoryImpl.kt
+++ data/src/main/java/com/hugidonic/data/repository/ScheduleRepositoryImpl.kt
@@ -1,124 +1,156 @@
 package com.hugidonic.data.repository
 
+import android.util.Log
+import com.hugidonic.data.converters.toModel
 import com.hugidonic.data.converters.toScheduleDayEntity
-import com.hugidonic.data.converters.toScheduleDayModel
 import com.hugidonic.data.converters.toSubjectEntity
 import com.hugidonic.data.converters.toSubjectModel
 import com.hugidonic.data.database.ScheduleDao
-import com.hugidonic.data.database.entities.ClassEntity
+import com.hugidonic.data.database.SubjectDao
 import com.hugidonic.data.remote.ApiService
-import com.hugidonic.data.remote.dto.SubjectDto
-import com.hugidonic.domain.models.ClassModel
+import com.hugidonic.data.remote.dto.ScheduleDayDto
 import com.hugidonic.domain.models.ScheduleDayModel
-import com.hugidonic.domain.models.SubjectModel
 import com.hugidonic.domain.repositories.ScheduleRepository
 import com.hugidonic.domain.utils.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Period
 import javax.inject.Inject
 
 class ScheduleRepositoryImpl @Inject constructor(
     private val scheduleDao: ScheduleDao,
+    private val subjectDao: SubjectDao,
     private val apiService: ApiService
 ) : ScheduleRepository {
 
-    override suspend fun getScheduleDayInfo(
-        dayOfWeek: String,
-    ): Flow<Resource<ScheduleDayModel>> = flow {
+    override suspend fun getWeekSchedule(
+        isFetchFromApi: Boolean,
+        typeOfWeek: String
+    ): Flow<Resource<List<ScheduleDayModel>>> = flow {
         emit(Resource.Loading(true))
 
-        val localScheduleInfo = scheduleDao.getScheduleDay(dayOfWeek)
-
-        localScheduleInfo?.let {
-            emit(
-                Resource.Success(
-                    data = localScheduleInfo.toScheduleDayModel()
-                )
+        val localWeekSchedule: List<ScheduleDayModel> = scheduleDao.getWeekScheduleByType(typeOfWeek = typeOfWeek).map {
+            ScheduleDayModel(
+                scheduleDayId = it.scheduleDayInfo.scheduleDayId,
+                dayOfWeek = it.scheduleDayInfo.dayOfWeek,
+                typeOfWeek = it.scheduleDayInfo.typeOfWeek,
+                date = it.scheduleDayInfo.date,
+                subjects = it.subjects.map { subjectEntity -> subjectEntity.toSubjectModel() }
             )
         }
+        Log.d("local", "$localWeekSchedule")
 
-        val remoteScheduleDayInfoEntity = try {
-            val scheduleDayDto = apiService.getScheduleDayInfo()
-            scheduleDayDto.toScheduleDayEntity()
+        emit(
+            Resource.Success(
+                data = localWeekSchedule
+            )
+        )
+
+        val shouldLoadFromCache = localWeekSchedule.isNotEmpty() && !isFetchFromApi
+        if (shouldLoadFromCache) {
+            Log.d("data", "Got data only from local db")
+            return@flow
+        }
+
+        try {
+            val weekScheduleDto: List<ScheduleDayDto> = if (typeOfWeek == "Чет") {
+                apiService.getChetWeekSchedule()
+            } else {
+                apiService.getNechetWeekSchedule()
+            }
+
+            Log.d("remote", "$weekScheduleDto")
+            saveWeekScheduleDtoToDb(weekScheduleDto = weekScheduleDto)
+        } catch (e: IOException) {
+            emit(Resource.Error(data = localWeekSchedule, message = "Something went wrong..."))
         } catch (e: HttpException) {
             emit(
                 Resource.Error(
-                    data = localScheduleInfo?.toScheduleDayModel(),
-                    message = "Couldn't load data, check your internet connection"
-                )
-            )
-            null
-        } catch (e: IOException) {
-            emit(Resource.Error(data = localScheduleInfo?.toScheduleDayModel(), message = "Couldn't load data"))
-            null
-        }
-
-        remoteScheduleDayInfoEntity?.let {
-            scheduleDao.insertScheduleDay(it)
-            emit(
-                Resource.Success(
-                    data = it.toScheduleDayModel()
+                    data = localWeekSchedule,
+                    message = "Couldn't load data from server. Check your internet connection"
                 )
             )
         }
-    }
 
-    override suspend fun getClasses(
-        dayOfWeek: String,
-    ): Flow<Resource<List<ClassModel>>> = flow {
-        emit(Resource.Loading(true))
+        val newWeekScheduleEntities = scheduleDao.getWeekScheduleByType(typeOfWeek = typeOfWeek)
+        Log.d("new local", "$newWeekScheduleEntities")
 
-        val localClasses = getLocalClasses(dayOfWeek = dayOfWeek)
-        emit(Resource.Success(data = localClasses))
-
-        val remoteSubjects = try {
-            apiService.getSubjects()
-        } catch (e: Exception) {
-            emit(Resource.Error(data = null, message = "Couldn't load data"))
-            null
-        }
-
-        remoteSubjects?.let {
-            saveSubjectsToDb(subjects = remoteSubjects, dayOfWeek = dayOfWeek)
-            val classesFromDb = getLocalClasses(dayOfWeek = dayOfWeek)
-            emit(Resource.Success(data = classesFromDb))
-        }
-    }
-
-    private suspend fun saveSubjectsToDb(subjects: List<SubjectDto?>, dayOfWeek: String) {
-        subjects.forEachIndexed { idx, subjectDto ->
-            subjectDto?.let {
-                val subjectEntity = subjectDto.toSubjectEntity()
-                // Save subject to db
-                scheduleDao.insertSubject(subjectEntity)
-
-                // Transform subject to class
-                val classEntity = ClassEntity(
-                    dayOfWeek = dayOfWeek,
-                    orderIndex = idx,
-                    subjectTitle = it.title
-                )
-                // Save class to db
-                scheduleDao.insertClass(classEntity)
-            }
-        }
-    }
-
-    private suspend fun getLocalClasses(dayOfWeek: String): List<ClassModel> {
-        val subjectsListFromDb = scheduleDao.getSubjectsAndClasses(dayOfWeek = dayOfWeek)
-        return subjectsListFromDb.map { subjectAndClass ->
-            ClassModel(
-                subject = subjectAndClass.subject.toSubjectModel(),
-                orderIndex = subjectAndClass.classEntity.orderIndex,
-                dayOfWeek = subjectAndClass.classEntity.dayOfWeek
+        val newWeekSchedule = newWeekScheduleEntities.map {
+            ScheduleDayModel(
+                scheduleDayId = it.scheduleDayInfo.scheduleDayId,
+                dayOfWeek = it.scheduleDayInfo.dayOfWeek,
+                typeOfWeek = it.scheduleDayInfo.typeOfWeek,
+                date = it.scheduleDayInfo.date,
+                subjects = it.subjects.map { subjectEntity -> subjectEntity.toSubjectModel() }
             )
         }
+
+        Log.d("localAfterRemote", "$newWeekSchedule")
+
+        emit(
+            Resource.Success(
+                data = newWeekSchedule
+            )
+        )
+        emit(Resource.Loading(false))
     }
 
-    override suspend fun getSubjectDetails(subjectTitle: String): SubjectModel {
-        val subjectEntity = scheduleDao.searchSubject(subjectTitle)
-        return subjectEntity.toSubjectModel()
+    override suspend fun getScheduleDayFromDB(typeOfWeek: String, dayOfWeek: String): ScheduleDayModel? {
+        val scheduleId = "${typeOfWeek}/${dayOfWeek}"
+        val scheduleDayEntity = scheduleDao.getScheduleDay(scheduleDayId = scheduleId)
+        return scheduleDayEntity?.toModel()
+    }
+
+    private suspend fun saveWeekScheduleDtoToDb(weekScheduleDto: List<ScheduleDayDto>) {
+        scheduleDao.clearScheduleDayTable(typeOfWeek = weekScheduleDto[0].typeOfWeek)
+
+        weekScheduleDto.forEach { scheduleDayDto ->
+            saveScheduleDayDtoToDb(scheduleDayDto = scheduleDayDto)
+            saveSubjectsDtoToDb(scheduleDayDto = scheduleDayDto)
+        }
+    }
+
+    private suspend fun saveScheduleDayDtoToDb(scheduleDayDto: ScheduleDayDto) {
+        val scheduleDayEntity = scheduleDayDto.toScheduleDayEntity()
+        scheduleDao.insertScheduleDay(scheduleDayEntity = scheduleDayEntity)
+    }
+
+    private suspend fun saveSubjectsDtoToDb(scheduleDayDto: ScheduleDayDto) {
+        scheduleDayDto.subjects.forEach { subjectDto ->
+            val subjectEntity =
+                subjectDto.toSubjectEntity(
+                    scheduleDayId = "${scheduleDayDto.typeOfWeek}/${scheduleDayDto.dayOfWeek}"
+                )
+            subjectDao.insertSubject(subjectEntity)
+        }
+    }
+
+    override fun getTypeOfWeek(): String {
+        val currentDate = getCurrentDate()
+
+        val semesterBeginDate = if (currentDate.monthValue in 2..6) {
+            LocalDate.of(currentDate.year, 2, 1)
+        } else {
+            LocalDate.of(currentDate.year, 9, 1)
+        }
+
+        val firstWeekMonday = semesterBeginDate.minusDays(semesterBeginDate.dayOfWeek.ordinal.toLong())
+
+        val periodDays = Period.between(firstWeekMonday, currentDate).days
+        val weeks = periodDays / 7
+
+        return if (weeks + 1 % 2 == 0) "чет" else "нечет"
+    }
+
+    override fun getCurrentDate(): LocalDate {
+        val today = LocalDate.now()
+        if (today.dayOfWeek != DayOfWeek.SUNDAY) {
+            return today
+        }
+        return today.plusDays(1)
     }
 }
