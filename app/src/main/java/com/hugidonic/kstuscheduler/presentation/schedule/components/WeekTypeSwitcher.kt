b3c1a3d637051b5114ed2c835d596a7bcf1589ee
Hugidonic <netvaca@ya.ru>
2023-09-14 13:45:18 +0300

    [dev]/{app}: WeekTypeSwitcher;

---
+++ app/src/main/java/com/hugidonic/kstuscheduler/presentation/schedule/components/WeekTypeSwitcher.kt
@@ -0,0 +1,68 @@
+package com.hugidonic.kstuscheduler.presentation.schedule.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hugidonic.kstuscheduler.presentation.ui.theme.MainAppTheme
+
+@Composable
+fun WeekTypeSwitcher(
+    modifier: Modifier = Modifier,
+    currentType: String,
+    onChangeType: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .clip(MaterialTheme.shapes.large)
+            .background(
+                if (isSystemInDarkTheme()) Color.DarkGray else Color.LightGray
+            )
+            .padding(horizontal = 20.dp, vertical = 6.dp)
+            .clickable {
+                onChangeType()
+            },
+    ) {
+        Text(
+            text = "${currentType}ная",
+            style = MaterialTheme.typography.body1,
+        )
+    }
+}
+
+@Composable
+private fun PreviewWeekTypeSwitcher() {
+    MainAppTheme {
+        Surface(
+            modifier = Modifier
+                .background(MaterialTheme.colors.surface)
+                .padding(15.dp)
+        ) {
+            WeekTypeSwitcher(currentType = "Чет", onChangeType = {})
+        }
+    }
+}
+
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark")
+@Composable
+fun PreviewWeekTypeSwitcherDark() {
+    PreviewWeekTypeSwitcher()
+}
+
+@Preview(name = "Light")
+@Composable
+fun PreviewWeekTypeSwitcherLight() {
+    PreviewWeekTypeSwitcher()
+}
