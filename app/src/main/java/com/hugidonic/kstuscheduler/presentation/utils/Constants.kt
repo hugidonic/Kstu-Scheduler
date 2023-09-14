7bdf60f903716af62caa7207860b0cea4401fcd0
Hugidonic <netvaca@ya.ru>
2023-09-14 13:41:41 +0300

    [dev]/{app}: DummyData, Constants contain WEEKDAYS_LIST

--- app/src/main/java/com/hugidonic/kstuscheduler/presentation/utils/Constants.kt
+++ app/src/main/java/com/hugidonic/kstuscheduler/presentation/utils/Constants.kt
@@ -1,15 +1,12 @@
 package com.hugidonic.kstuscheduler.presentation.utils
 
-import com.hugidonic.domain.models.Timing
-
 val ACTIVE_IDX = 0
 
-val TIMINGS = listOf<Timing>(
-    Timing(start = "8:00", end = "9:40"),
-    Timing(start = "9:50", end = "11:10"),
-    Timing(start = "11:20", end = "12:50"),
-    Timing(start = "13:00", end = "14:30"),
-    Timing(start = "14:40", end = "16:10"),
-    Timing(start = "16:20", end = "17:50"),
-    Timing(start = "18:00", end = "19:30"),
+val WEEKDAYS_LIST = arrayListOf(
+    "Пн",
+    "Вт",
+    "Ср",
+    "Чт",
+    "Пт",
+    "Сб",
 )
