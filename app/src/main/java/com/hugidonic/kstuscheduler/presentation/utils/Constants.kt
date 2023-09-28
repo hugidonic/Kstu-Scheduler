374563d4172a76539704470f69ea8ccd0973272c
Hugidonic <netvaca@ya.ru>
2023-09-28 13:33:28 +0300

    [dev]/{app}: Constants and DummyData updated

--- app/src/main/java/com/hugidonic/kstuscheduler/presentation/utils/Constants.kt
+++ app/src/main/java/com/hugidonic/kstuscheduler/presentation/utils/Constants.kt
@@ -1,12 +1,31 @@
 package com.hugidonic.kstuscheduler.presentation.utils
 
-val ACTIVE_IDX = 0
+import androidx.compose.ui.unit.dp
+import com.hugidonic.kstuscheduler.R
+import com.hugidonic.kstuscheduler.presentation.navigation.bottombar.BottomNavItem
 
-val WEEKDAYS_LIST = arrayListOf(
-    "Пн",
-    "Вт",
-    "Ср",
-    "Чт",
-    "Пт",
-    "Сб",
-)
+object Constants {
+    val WEEKDAYS_LIST = arrayListOf(
+        "Пн",
+        "Вт",
+        "Ср",
+        "Чт",
+        "Пт",
+        "Сб",
+    )
+
+    val BottomNavigationHeight = 56.dp
+
+    val BottomNavItems = listOf(
+        BottomNavItem(
+            label = "Расписание",
+            icon = R.drawable.schedule,
+            route = "schedule"
+        ),
+        BottomNavItem(
+            label = "Профиль",
+            icon = R.drawable.user,
+            route = "profile"
+        ),
+    )
+}
