5eab8c24ed13a00612b0882ef7c8080c70952e0b
Hugidonic <netvaca@ya.ru>
2023-09-30 18:53:13 +0300

    [dev]/{app}: SplashScreen and other

--- app/src/main/java/com/hugidonic/kstuscheduler/presentation/MainActivity.kt
+++ app/src/main/java/com/hugidonic/kstuscheduler/presentation/MainActivity.kt
@@ -7,11 +7,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.hugidonic.kstuscheduler.presentation.navigation.AppNavGraph
-import com.hugidonic.kstuscheduler.presentation.navigation.bottombar.BottomNavigationBar
+import com.hugidonic.kstuscheduler.presentation.navigation.Screen
+import com.hugidonic.kstuscheduler.presentation.navigation.topbar.TopBar
 import com.hugidonic.kstuscheduler.presentation.ui.theme.MainAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,9 +33,29 @@ class MainActivity : ComponentActivity() {
 					color = MaterialTheme.colors.background
 				) {
 					val navController = rememberNavController()
+
+					val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+					var isTopBarVisible by rememberSaveable { (mutableStateOf(true)) }
+
+					// Control TopBar and BottomBar
+					when (navBackStackEntry?.destination?.route) {
+						Screen.SubjectDetails.route, Screen.PrepodDetails.route -> {
+							isTopBarVisible = true
+						}
+
+						else -> {
+							isTopBarVisible = false
+						}
+					}
+
 					Scaffold(
 						drawerElevation = 0.dp,
-						bottomBar = { BottomNavigationBar(navController = navController) },
+						topBar = {
+							if (isTopBarVisible) {
+								TopBar(navController = navController)
+							}
+						},
 					) {padding ->
 						AppNavGraph(navHostController = navController, padding = padding)
 					}
@@ -37,6 +63,24 @@ class MainActivity : ComponentActivity() {
 			}
 		}
 	}
+}
+
+@Stable
+@Composable
+private fun NavHostController.shouldShowBottomNavigationAsState(): State<Boolean> {
+	val shouldShowBottomNavigation = remember { mutableStateOf(false) }
+
+	DisposableEffect(this) {
+		val listener = NavController.OnDestinationChangedListener { _, destination, _ ->
+			shouldShowBottomNavigation.value = destination.route != Screen.Splash.route
+		}
+		addOnDestinationChangedListener(listener)
+		onDispose {
+			removeOnDestinationChangedListener(listener)
+		}
+	}
+
+	return shouldShowBottomNavigation
 }
 
 
